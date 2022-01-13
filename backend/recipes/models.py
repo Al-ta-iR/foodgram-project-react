634af158a3eb626a611c2ec8cfb909d2b6fd810e
Al-ta-iR <artyom.oleynik@gmail.com>
2022-01-13 11:30:29 +0300

    Add Ingredient model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    measurement_unit = models.CharField(
+        max_length=200,
+        verbose_name='Единица измерения'
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
