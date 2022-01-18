ed48f3edf3f8ccf4151b7052e978556be5cb8bd8
Al-ta-iR <artyom.oleynik@gmail.com>
2022-01-18 16:31:12 +0300

    Add Recipe model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,9 @@
 from colorfield.fields import ColorField
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
+
+User = get_user_model()
 
 
 class Ingredient(models.Model):
@@ -45,3 +49,73 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор публикации'
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        verbose_name='Картинка'
+    )
+    text = models.TextField(
+        verbose_name='Текстовое описание'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name='Ингредиенты'
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        verbose_name='Теги'
+    )
+    cooking_time = models.IntegerField(
+        validators=[MinValueValidator(1)],
+        default=1,
+        verbose_name='Время приготовления в минутах'
+    )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients',
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients',
+        verbose_name='Ингредиент'
+    )
+    amount = models.IntegerField(
+        validators=[MinValueValidator(1)],
+        default=1,
+        verbose_name='Количество'
+    )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецептов'
+
+    def __str__(self):
+        return '{} - {}'.format(self.ingredient, self.amount)
