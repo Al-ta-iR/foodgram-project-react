6b2c4a7e6442b2b196b6fce13102a42fce70f6de
Al-ta-iR <artyom.oleynik@gmail.com>
2022-01-21 21:07:16 +0300

    Add handlers for unsafe methods in recipe requests

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer, UserSerializer
-from rest_framework.serializers import ModelSerializer, ReadOnlyField
+from drf_extra_fields.fields import Base64ImageField
+from rest_framework.serializers import ModelSerializer, ReadOnlyField, ValidationError
 
 from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
 
@@ -49,8 +50,79 @@ class RecipeSerializer(ModelSerializer):
         read_only=True,
         many=True
     )
+    image = Base64ImageField()
 
     class Meta:
         model = Recipe
         fields = ('id', 'tags', 'author', 'ingredients', 'name',
                   'image', 'text', 'cooking_time')
+
+    def validate_ingredients(self, ingredients):
+        if not ingredients:
+            raise ValidationError('В рецепте не заполнены ингредиенты!')
+        return ingredients
+
+    def validate_tags(self, tags):
+        if not tags:
+            raise ValidationError('В рецепте не заполнены теги!')
+        return tags
+
+    def validate_image(self, image):
+        if not image:
+            raise ValidationError('Добавьте картинку рецепта!')
+        return image
+
+    def validate_name(self, name):
+        if not name:
+            raise ValidationError('Не заполнено название рецепта!')
+        if self.context.get('request').method == 'POST':
+            current_user = self.context.get('request').user
+            if Recipe.objects.filter(author=current_user, name=name).exists():
+                raise ValidationError(
+                    'Рецепт с таким названием у вас уже есть!'
+                )
+        return name
+
+    def validate_text(self, text):
+        if not text:
+            raise ValidationError('Не заполнено описание рецепта!')
+        return text
+
+    def validate_cooking_time(self, cooking_time):
+        if not cooking_time:
+            raise ValidationError('Не заполнено время приготовления рецепта!')
+        return cooking_time
+
+    def create_recipe_ingredients(self, ingredients, recipe):
+        for i in ingredients:
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient_id=i.get('id'),
+                amount=i.get('amount'),
+            )
+
+    def create(self, validated_data):
+        ingredients = self.validate_ingredients(
+            self.initial_data.get('ingredients')
+        )
+        tags = self.validate_tags(
+            self.initial_data.get('tags')
+        )
+        recipe = Recipe.objects.create(**validated_data)
+        self.create_recipe_ingredients(
+            ingredients,
+            recipe
+        )
+        recipe.tags.set(tags)
+        return recipe
+
+    def update(self, recipe, validated_data):
+        ingredients = self.initial_data.get('ingredients')
+        tags = self.initial_data.get('tags')
+        recipe = super().update(recipe, validated_data)
+        if ingredients:
+            recipe.ingredients.clear()
+            self.create_recipe_ingredients(ingredients, recipe)
+        if tags:
+            recipe.tags.set(tags)
+        return recipe
