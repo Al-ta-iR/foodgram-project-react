7ad0b5a28c6acb04d57e5cd62c9a20ab2da5904d
Al-ta-iR <artyom.oleynik@gmail.com>
2022-01-31 18:34:07 +0300

    Setting infra

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='Django_SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost, backend').split(', ')
 
 
 INSTALLED_APPS = [
